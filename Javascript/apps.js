1314426b3631e597a3e06bdfb0a0b28102c483b7
Steve Brayton Gongora Luevano <steve.gongoralo@uanl.edu.mx>
2021-05-14 16:02:04 -0500

    add archivo js

---
+++ Javascript/apps.js
@@ -0,0 +1,32 @@
+/*  Obtener La Hora */
+let fechaActual = new Date();
+let hora = fechaActual.getHours();
+
+
+/* Poner Modo Oscuro Manual */
+const btnSwitch =document.querySelector('#switch');
+btnSwitch.addEventListener('click',() =>{
+    document.body.classList.toggle('dark')
+});
+
+
+/* Poner Modo Oscuro Automatico */
+if(hora<=6 || hora>=19){
+    document.body.classList.toggle('dark')
+}
+else{
+    document.body.classList.toggle('.')
+}
+
+
+/*  Saludar en Pagina Contatco */
+let palabras=document.getElementsByClassName('saludo');
+if (hora>=6 && hora<12){
+    palabras[0].textContent="BUENOS DIAS"
+}
+else if (hora<19){
+    palabras[0].textContent="BUENAS TARDES"
+}
+else{
+    palabras[0].textContent="BUENAS NOCHES"
+}
